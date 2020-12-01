ca675b7076f3ee1c7c46552bb7bc843f79414e33
lorakirana <71301039+lorakirana@users.noreply.github.com>
2020-12-01 12:55:58 +0700

    Delete beli.php

--- beli.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $conn = new mysqli("localhost", "root", "", "assessment2Lora");
-    $sql = "SELECT * FROM menu WHERE id = '$id'";
-    $result = $conn->query($sql);
-    while ($row=$result->fetch_assoc()) {
-        $harga = $row['harga_menu'];
-    }
-
- ?>
